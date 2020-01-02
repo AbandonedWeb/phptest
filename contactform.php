55d733a4f2195db2c43967f7a7b030b3860bd021
Megli <meglipanci12@gmail.com>
2020-01-02 23:56:19 +0100

    g
    g

--- contactform.php
+++ contactform.php
@@ -1,24 +1,16 @@
-<?php 
+<?php
 
 if (isset($_POST['submit'])) {
+$name = $_POST['name'];
+$subject = $_POST['subject'];
+$mailFrom = $_POST['mail'];
+$message = $_POST['message'];
 
-	$name = $_POST['name'];
-	$subject = $_POST['subject'];
-	$mailFrom = $_POST['mail'];
-	$message = $_POST['message'];
- 
-    $mailTo = "meglipanci100@protonmail.com";
-    $headers = "From: ".$mailFrom;
-    $txt = "You have received an e-mail from ".$name.".\n\n".$message; 
+$mailTo = "meglipanci12@gmail.com";
+$headers = "From: ".$mailFrom;
+$txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
-
-
-   mail($mailTo, $subject, $txt, $headers);
-   header("Location: index.php?mailsend");
-
-
-
+mail($mailTo, $subject, $txt, $headers);
+header("Location: index.php?mailsend");
 }
-
-
  
