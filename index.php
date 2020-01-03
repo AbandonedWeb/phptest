0feeea68fef85a7b0f84f167a6fc2babbbcaa0ea
Megli <meglipanci12@gmail.com>
2020-01-03 02:57:31 +0100

    g
    g

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+
+<main>
+	<form class="contact-form" action="contactform.php" method="post">
+		
+<input type="text" name="name" placeholder="Full name">
+<input type="text" name="mail" placeholder="Your e-mail">
+<input type="" name="subject" placeholder="Subject">
+<textarea name="message" rows="8" placeholder="Message" cols="80">  </textarea>
+<button type="submit" name="submit">Send email</button>
+
+	</form>
+	</main>
+
+</body>
+</html>
